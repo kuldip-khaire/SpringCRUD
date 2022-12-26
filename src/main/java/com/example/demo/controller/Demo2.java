26e5ccae3b87f98d82b156471ae1cbf7d9c4baf9
Kuldip <kuldipkhaire27@gmail.com>
2022-12-26 19:08:40 +0530

    commit

--- src/main/java/com/example/demo/controller/Demo2.java
+++ src/main/java/com/example/demo/controller/Demo2.java
@@ -1,3 +1,9 @@
+package com.example.demo.controller;
+
 public class Demo2{
- // 
+
+	public static void main(String[] args) {
+		System.out.println("premaster & sub branch in same file -local repo");
+	}
+	
 }
