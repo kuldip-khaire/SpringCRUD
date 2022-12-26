6895de38aa8e2c9bc333491e3e6f049bf9720ca5
kuldip-khaire <121336758+kuldip-khaire@users.noreply.github.com>
2022-12-26 19:18:28 +0530

    Update Demo2.java

--- src/main/java/com/example/demo/controller/Demo2.java
+++ src/main/java/com/example/demo/controller/Demo2.java
@@ -6,7 +6,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("premaster & sub branch in same file -local repo");
 
-		System.out.println("2");
+		System.out.println("3");
 
 		System.out.println("premaster & sub branch in same file -remote repo");
 	}
